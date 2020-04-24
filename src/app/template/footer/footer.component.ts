5115888f693c8f078ad67774af011e8f5ec8ee6e
Sumit Dey <sumitdeyonline@gmail.com>
2020-04-23 22:21:00 -0700

    UI Changes

--- src/app/template/footer/footer.component.ts
+++ src/app/template/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/authentication/auth.service';
+import { EMAIL_CONFIG } from 'src/app/global-config';
 
 @Component({
   selector: 'footer',
@@ -7,10 +8,11 @@ import { AuthService } from 'src/app/services/authentication/auth.service';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  email: string;
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    this.email = "mailto:"+EMAIL_CONFIG.HelpEmail+"?Subject=Feedback";
   }
 
 }
